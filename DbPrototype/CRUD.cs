a8a636c47c42eef7ee1ef2017b94d8b0b2060eb5
AntonioSantanaCode <71064079+AntonioSantanaCode@users.noreply.github.com>
2021-02-10 13:34:11 +0100

    Made Create in (CRUD class)

---
+++ DbPrototype/CRUD.cs
@@ -0,0 +1,48 @@
+﻿using DbPrototype.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbPrototype
+{
+    public class CRUD
+    {
+        public static void CreateProduct()
+        {
+            using (var context = new AppDbContext())
+            {
+                Product product1 = new Product();
+
+                Console.WriteLine("**********Enter Product details**********");
+                Console.WriteLine("----------------------------------------");
+                Console.Write("Product Name : ");
+                product1.Name = Console.ReadLine();
+
+                Console.Write("Product Price : ");
+                product1.Price = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Product Weight : ");
+                product1.Weight = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Product Description : ");
+                product1.Description = Console.ReadLine();
+
+                product1.Image = null;
+
+                Console.Write("Category : ");
+                product1.Category = Console.ReadLine();
+
+                product1.CreateDate = DateTime.Now;
+
+                Console.Write("How many : ");
+                product1.Stock = int.Parse(Console.ReadLine());
+
+                Console.Write("Size : ");
+                product1.Size = Console.ReadLine();
+
+                context.Add(product1);
+                context.SaveChanges();
+            }
+        }
+    }
+}
