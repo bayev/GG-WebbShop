0ccbc5455d0ce0b9b4deea21242ccb926015b72f
Mahesa-Git <71064153+Mahesa-Git@users.noreply.github.com>
2021-02-25 15:53:05 +0100

    Searchbar fixed
    Users now can search for products using the searchbar in _layout. JavaScript is used to send relevant data in the url from _layout to the code-behind in the QueryRazorPage.

--- GG-Webbshop/Pages/SearchResult.cshtml.cs
+++ GG-Webbshop/Pages/SearchResult.cshtml.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RestSharp;
-using System;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GG_Webbshop.Pages
@@ -10,43 +8,52 @@ namespace GG_Webbshop.Pages
     public class SearchResultModel : PageModel
     {
         [BindProperty(SupportsGet = true)]
-        public string QueryString { get; set; }
+        public string Id { get; set; }
         [BindProperty(SupportsGet = true)]
-        public AllProductsResponseModel Product { get; set; }
+        public AllProductsResponseModel[] Product { get; set; }
+        public string Message { get; set; }
+        public int ProductResultCount { get; set; }
         public SearchResultModel()
         {
         }
 
-        public async Task<IActionResult> OnGetAsync(string queryString) //HÄMTA PRODUCT HÄR
+        public async Task<IActionResult> OnGetAsync() //HÄMTA PRODUCT HÄR
         {
-            queryString = QueryString;
-            if (queryString == null)
+            RestClient client;
+            if (string.IsNullOrEmpty(Id))
+            {
+                client = new RestClient($"https://localhost:44309/query/all");
+            }
+            else
+            {
+                client = new RestClient($"https://localhost:44309/query/search/{Id}");
+            }
+
+            
+            RestRequest request = new RestRequest
+            {
+                Method = Method.GET
+            };
+            request.Parameters.Clear();
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var model = AllProductsResponseModel.FromJson(response.Content);
+                Product = model;
+                ProductResultCount = Product.Length;
+                if (Product.Length == 0)
+                {
+                    Message = "Inga produkter hittade";
+                    ProductResultCount = 0;
+                }
+
+            }
+            else
             {
                 return NotFound();
             }
-            //byte[] tokenByte;
-            //HttpContext.Session.TryGetValue(ToolBox.TokenName, out tokenByte);
-            //string token = Encoding.ASCII.GetString(tokenByte);
-
-                RestClient client = new RestClient($"https://localhost:44309/query/search/{queryString}");
-                RestRequest request = new RestRequest
-                {
-                    Method = Method.GET
-                };
-                request.Parameters.Clear();
-
-                IRestResponse response = client.Execute(request);
-
-                if (response.StatusCode == System.Net.HttpStatusCode.OK)
-                {
-                    var model = AllProductsResponseModel.FromJsonSingle(response.Content);
-                    Product = model;
-                }
-                else
-                {
-                    return NotFound();
-                }
-            
             return Page();
         }
     }
--- GG-Webbshop/wwwroot/js/site.js
+++ GG-Webbshop/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function setUrl() {
+    window.location.href = 'SearchResult?id=' + document.getElementById('searchInput').value;
+};
