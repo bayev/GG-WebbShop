e36bb66ebbaf271f39ca97473c5a71f64a70b4dc
johnsjoo <70665563+johnsjoo@users.noreply.github.com>
2021-02-26 17:24:03 +0100

    Added code to _Layout and TokenChecker Class
    The code is commented, need to get fixed

---
+++ GG-Webbshop/Pages/TokenChecker.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GG_Webbshop.Pages
+{
+    public class TokenChecker : PageModel
+    {
+        //public bool CheckIfValid()
+        //{
+
+        //    bool validToken;
+            
+        //    byte[] tokenByte;
+        //    HttpContext.Session.TryGetValue(ToolBox.TokenName, out tokenByte);
+        //    string token = Encoding.ASCII.GetString(tokenByte);
+
+        //    if (!String.IsNullOrEmpty(token))
+        //        validToken = true;
+        //    else
+        //        validToken = false;
+
+        //    return validToken;
+        //}
+        //public IActionResult LogOutSessionClear()
+        //{
+
+        //    HttpContext.Session.Clear();
+        //    return RedirectToPage("/index");
+        //}
+
+    }
+}
