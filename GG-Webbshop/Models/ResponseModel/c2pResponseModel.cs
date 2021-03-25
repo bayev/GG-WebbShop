3f5251b8ac35b15b9a60656e6e2dff6166da30a2
Mahesa-Git <71064153+Mahesa-Git@users.noreply.github.com>
2021-03-26 00:44:43 +0100

    Added functionality Cart
    You are now able to put stuff in the cart and also able to delete products from it.

---
+++ GG-Webbshop/Models/ResponseModel/c2pResponseModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace GG_Webbshop.Models.ResponseModel
+{
+    
+
+    public partial class c2pResponseModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("weight")]
+        public decimal Weight { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("createDate")]
+        public DateTime CreateDate { get; set; }
+
+        [JsonProperty("size")]
+        public string Size { get; set; }
+
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+
+        [JsonProperty("amount")]
+        public int Amount { get; set; }
+
+        [JsonProperty("c2pID")]
+        public string C2PId { get; set; }
+    }
+    public partial class c2pResponseModel
+    {
+        public static c2pResponseModel[] FromJson(string json) => JsonConvert.DeserializeObject<c2pResponseModel[]>(json, GG_Webbshop.Converter.Settings);
+        public static c2pResponseModel FromJsonSingle(string json) => JsonConvert.DeserializeObject<c2pResponseModel>(json, GG_Webbshop.Converter.Settings);
+    }
+}
--- GG-Webbshop/Pages/CartView.cshtml.cs
+++ GG-Webbshop/Pages/CartView.cshtml.cs
@@ -1,16 +1,101 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GG_Webbshop.Models.ResponseModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GG_Webbshop.Pages
 {
     public class CartViewModel : PageModel
     {
-        public void OnGet()
+        [BindProperty(SupportsGet = true)]
+        public string Id { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public c2pResponseModel[] c2pRM { get; set; }
+        [BindProperty]
+        public string Message { get; set; }
+        [BindProperty]
+        public string Message2 { get; set; }
+        public async Task<IActionResult> OnGetAsync()
         {
+            
+            string token = null;
+            try
+            {
+                byte[] tokenByte;
+                HttpContext.Session.TryGetValue(ToolBox.TokenName, out tokenByte);
+                token = Encoding.ASCII.GetString(tokenByte);
+            }
+            catch (Exception)
+            {
+                Message = "Du måste logga in för att kunna se dina valda varor.";
+                return Page();
+            }
+            if (Id != null)
+            {
+                RestClient client = new RestClient($"https://localhost:44309/cart/deletefromcart/{Id}");
+                RestRequest request = new RestRequest
+                {
+                    Method = Method.DELETE
+                };
+                request.Parameters.Clear();
+                request.AddHeader("Authorization", $"bearer {token}");
+
+                IRestResponse response = client.Execute(request);
+                string responseContent = response.Content;
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return RedirectToPage("./cartview");
+                }
+                if(response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    Message2 = "Vi kunde inte radera produkten från din varukorg, vänligen kontakta administratören";
+                    return Page();
+                }
+                if(response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    Message = "Du som användare hittades inte i vår databas. Vänligen kontakta administratören";
+                    return Page();
+                }
+            }
+            if (!String.IsNullOrEmpty(token))
+            {
+                RestClient client = new RestClient($"https://localhost:44309/cart/getcart");
+                RestRequest request = new RestRequest
+                {
+                    Method = Method.GET
+                };
+                request.Parameters.Clear();
+                request.AddHeader("Authorization", $"bearer {token}");
+
+                IRestResponse response = client.Execute(request);
+                string responseContent = response.Content;
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var model = c2pResponseModel.FromJson(response.Content);
+                    c2pRM = model;
+                    return Page();
+                }
+                if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    Message = "din varukorg är tom!";
+                    return Page();
+                }
+                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    Message = "Användaren kunde inte hittas. Prova att logga in och ut igen!";
+                }
+                else
+                {
+                    Message = "Något gick heltokigt! kontakta administratören";
+                    return Page();
+                }
+            }
+            return Page();
         }
     }
 }
